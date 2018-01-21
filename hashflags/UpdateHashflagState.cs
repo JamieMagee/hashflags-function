e666f0cf0bb64a8e8ed081249cfdcbd1abdd8662
Jamie Magee <jamie.magee@gmail.com>
2018-01-21 22:32:32 +0100

    Add functions to store hashflags and create hero images

--- hashflags/UpdateHashflagState.cs
+++ hashflags/UpdateHashflagState.cs
@@ -11,14 +11,16 @@ using Newtonsoft.Json.Linq;
 
 namespace hashflags
 {
-    public static class StoreHashflags
+    public static class UpdateHashflagState
     {
-        [FunctionName("StoreHashflags")]
+        [FunctionName("UpdateHashflagState")]
         [StorageAccount("AzureWebJobsStorage")]
         public static void Run(
             [TimerTrigger("0 0 * * * *")] TimerInfo timer,
             [Blob("json/activeHashflags", FileAccess.ReadWrite)] CloudBlockBlob initDataBlob,
             [Table("hashflags", "active")] CloudTable table,
+            [Queue("save-hashflags")] ICollector<KeyValuePair<string, string>> collector,
+            [Queue("create-hero")] ICollector<KeyValuePair<string, string>> collector2,
             TraceWriter log)
         {
             log.Info($"Function executed at: {DateTime.Now}");
@@ -27,7 +29,9 @@ namespace hashflags
             var initData = JObject.Parse(initDataJson);
             var activeHashflags = initData["activeHashflags"].ToObject<Dictionary<string, string>>();
 
-            var tableQuery = new TableQuery<HashFlag>();
+            table.CreateIfNotExists();
+
+            var tableQuery = new TableQuery<HashFlag>().Where(TableQuery.GenerateFilterCondition("PartitionKey", "eq", "active"));
             var previousHashflags = table.ExecuteQuery(tableQuery);
 
             var previousHashtags = previousHashflags.Select(x => x.HashTag);
@@ -44,22 +48,18 @@ namespace hashflags
             {
                 log.Info($"NEW: {entry}");
                 var hf = activeHashflags.First(x => x.Key == entry);
-                InsertNew(hf, table);
-            }
-
-            foreach (var entry in currentHashtags.Intersect(previousHashtags))
-            {
-                log.Info($"ACTIVE: {entry}");
+                InsertNew(hf, table, log);
+                collector.Add(hf);
+                collector2.Add(hf);
             }
         }
 
         private static void MovePartition(HashFlag hf, CloudTable table)
         {
-            var batch = new TableBatchOperation();
-            batch.Delete(hf);
-            batch.Insert(new HashFlag()
+            var delete = TableOperation.Delete(hf);
+            var insert = TableOperation.Insert(new HashFlag()
             {
-                PartitionKey = "Inactive",
+                PartitionKey = "inactive",
                 RowKey = hf.RowKey,
                 HashTag = hf.HashTag,
                 Path = hf.Path,
@@ -67,18 +67,20 @@ namespace hashflags
                 LastSeen = DateTime.Now.Date
             });
 
-            table.ExecuteBatch(batch);
+            table.Execute(delete);
+            table.Execute(insert);
         }
 
-        private static void InsertNew(KeyValuePair<string, string> hf, CloudTable table)
+        private static void InsertNew(KeyValuePair<string, string> hf, CloudTable table, TraceWriter log)
         {
             var insert = TableOperation.Insert(new HashFlag
             {
-                RowKey = "Active",
-                PartitionKey = hf.Key + hf.Value.Replace('/', '_'),
+                PartitionKey = "active",
+                RowKey = hf.Key + hf.Value.Replace('/', '_').Split('.')[0],
                 HashTag = hf.Key,
                 Path = hf.Value,
-                FirstSeen = DateTime.Now.Date
+                FirstSeen = DateTime.Now.Date,
+                LastSeen = DateTime.Now.Date,
             });
 
             table.Execute(insert);
@@ -87,8 +89,8 @@ namespace hashflags
 
     public class HashFlag : TableEntity
     {
-        public string HashTag { get; set; }
-        public string Path { get; set; }
+        public String HashTag { get; set; }
+        public String Path { get; set; }
         public DateTime FirstSeen { get; set; }
         public DateTime LastSeen { get; set; }
     }
