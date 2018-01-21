5a8052c6bfb7d2570b42d8e279cf952215f82e81
Jamie Magee <jamie.magee@gmail.com>
2018-01-22 00:00:36 +0100

    Initial code for twitter bot

---
+++ hashflags/TweetHashflag.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+namespace hashflags
+{
+    class TweetHashflag
+    {
+
+        [FunctionName("CreateHeroImage")]
+        [StorageAccount("AzureWebJobsStorage")]
+        public static void Run(
+            [QueueTrigger("tweet")] KeyValuePair<string, string> hf,
+            [Blob("heroimages")] CloudBlobContainer heroContainer,
+            TraceWriter log)
+        {
+            log.Info($"Function executed at: {DateTime.Now}");
+
+            
+        }
+}
