abeae359c86193a2a5a3f17a6634ea02b7be8e72
Jamie Magee <jamie.magee@gmail.com>
2018-01-22 00:41:23 +0100

    Bit more work on TweetHashflag

--- hashflags/TweetHashflag.cs
+++ hashflags/TweetHashflag.cs
@@ -1,23 +1,50 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.WindowsAzure.Storage.Blob;
+using Tweetinvi;
+using Tweetinvi.Logic;
+using Tweetinvi.Models;
+using User = Tweetinvi.User;
 
 namespace hashflags
 {
     class TweetHashflag
     {
 
-        [FunctionName("CreateHeroImage")]
+        [FunctionName("TweetHashflag")]
         [StorageAccount("AzureWebJobsStorage")]
         public static void Run(
-            [QueueTrigger("tweet")] KeyValuePair<string, string> hf,
+            [TimerTrigger("0 * * * * *")] TimerInfo timer,
+            [Queue("tweet")] KeyValuePair<string, string> hf,
             [Blob("heroimages")] CloudBlobContainer heroContainer,
             TraceWriter log)
         {
             log.Info($"Function executed at: {DateTime.Now}");
 
-            
+            var authenticatedUser = InitialiseTwitter();
+
+            var hashflagBlob = heroContainer.GetBlockBlobReference(hf.Key);
+            IMedia media;
+            using (var stream = new MemoryStream())
+            {
+                hashflagBlob.DownloadToStream(stream);
+                media = Upload.UploadImage(stream.ToArray());
+            }
         }
+
+        private static AuthenticatedUser InitialiseTwitter()
+        {
+            var consumerKey = GetEnvironmentVariable("CONSUMER_KEY");
+            var consumerSecret = GetEnvironmentVariable("CONSUMER_SECRET");
+            var accessToken = GetEnvironmentVariable("ACCESS_TOKEN");
+            var accessTokenSecret = GetEnvironmentVariable("ACCESS_TOKEN_SECRET");
+            var userCredentials = Auth.CreateCredentials(consumerKey, consumerSecret, accessToken, accessTokenSecret);
+            return User.GetAuthenticatedUser(userCredentials) as AuthenticatedUser;
+        }
+
+        private static string GetEnvironmentVariable(string name) => Environment.GetEnvironmentVariable(name, EnvironmentVariableTarget.Process);
+    }
 }
